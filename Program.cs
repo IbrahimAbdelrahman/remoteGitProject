6340486019763a5c76507d03810eb4d5064bc38e
ibrahim abdelrahman <ibrahim.a.khaleefa@gmail.com>
2019-12-18 17:32:53 +0200

    update two

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World 1!");
+            Console.WriteLine("Hello World 2!");
         }
     }
 }
